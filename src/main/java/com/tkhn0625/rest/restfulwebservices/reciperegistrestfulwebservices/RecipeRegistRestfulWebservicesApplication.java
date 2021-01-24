0af2bad55fa0d47c00ffde565e12657ec5ab1a07
tkhn0625 <athleta.0315@gmail.com>
2021-01-24 10:42:44 +0900

    initial commit

---
+++ src/main/java/com/tkhn0625/rest/restfulwebservices/reciperegistrestfulwebservices/RecipeRegistRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.tkhn0625.rest.restfulwebservices.reciperegistrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRegistRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeRegistRestfulWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkhn0625/rest/restfulwebservices/reciperegistrestfulwebservices/RecipeRegistRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkhn0625.rest.restfulwebservices.reciperegistrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeRegistRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
